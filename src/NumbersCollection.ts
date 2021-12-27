ab736eebeb535e6930b5fa02d53f8cd6bbc7eaa5
arr13 <abubjazov@mail.ru>
2021-12-28 02:16:59 +0300

    app: Sorter: refactor

---
+++ src/NumbersCollection.ts
@@ -0,0 +1,18 @@
+export class NumbersCollection {
+    constructor(public data: number[]) {}
+
+    get length(): number {
+        return this.data.length
+    }
+
+    compare(leftIdx: number, rightIdx: number): boolean {
+        return this.data[leftIdx] > this.data[rightIdx]        
+    }
+
+    swap(leftIdx: number, rightIdx: number): void {
+        const tmp = this.data[leftIdx]
+
+        this.data[leftIdx] = this.data[rightIdx]
+        this.data[rightIdx] = tmp
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,13 @@
+import { NumbersCollection } from "./NumbersCollection"
+import { Sorter } from "./Sorter"
 
-class Sorter {
-    constructor(public collection: number[] | string) {}
-
-    sort(): void {
-        const { length } = this.collection
-
-        for (let i = 0; i < length; i++) {
-
-            for (let j = 0; j < length - i - 1; j++) {
-
-                if (this.collection instanceof Array) {
-                    if (this.collection[j] > this.collection[j + 1]) {
-                        const leftHand = this.collection[j]
-    
-                        this.collection[j] = this.collection[j + 1]
-                        this.collection[j + 1] = leftHand
-                    } 
-                }
-
-                if (typeof this.collection === 'string') {
-                    const arr = this.collection.split('')
-
-                    if (arr[j] > arr[j + 1]) {
-                        const leftHand = arr[j]
-    
-                        arr[j] = arr[j + 1]
-                        arr[j + 1] = leftHand
-                    }
-
-                    this.collection = arr.join('')
-                }        
-            }
-        }
-    }
-}
-
-const sorter = new Sorter([4, -3, 11, 2])
+const numbersCollection = new NumbersCollection([4, -3, 11, 2])
+const sorter = new Sorter(numbersCollection)
 
 sorter.sort()
-console.log(sorter.collection)
+console.log(numbersCollection.data)
 
-const sorterStr = new Sorter('ZbacX')
+// const sorterStr = new Sorter('ZbacX')
 
-sorterStr.sort()
-console.log(sorterStr.collection)
+// sorterStr.sort()
+// console.log(sorterStr.collection)
